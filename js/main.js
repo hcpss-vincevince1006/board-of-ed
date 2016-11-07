f4e80295bdaf0544804bfaab6757515f1acc0472
hcpss-vincevince1006 <vincent_dubay@hcpss.org>
2016-11-07 12:06:43 -0500

    Homepage and navigation examples working state

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+
+var navToggle = function(clickElement) {
+  $("#accordion-mobile h3").click(function(){
+    //slide up all the link lists
+    $("#accordion-mobile ul ul").slideUp(300);
+    //slide down the link list below the h3 clicked - only if its closed  
+    if(!$(this).next().is(":visible")) {
+      $(this).next().slideDown(300);
+    }
+  })
+}
+
+navToggle('#accordion-mobile h3');
+
+var sectionArrowToggle = function(clickElement) {
+  $(clickElement).on('click', function(event){
+    if($(this).next().hasClass('hide')) {
+        $(this).next().slideToggle(0,function(){
+          $(this).removeClass('hide').slideDown(300);
+        });
+      $(this).find('.fa-angle-right').css({
+            "transform" : "rotate(90deg)",
+            "transition-timing-function" : "cubic-bezier(0.25, 0.1, 0.25, 1)",
+            "transition-duration" : "0.3s"
+        });
+    } else {
+        $(this).next().slideToggle(300,function(){
+          $(this).addClass('hide').removeAttr('style');
+        });
+      $(this).find('.fa-angle-right').css("transform", "none");
+    }
+  });
+}
+sectionArrowToggle('#list h3');
+sectionArrowToggle('#list h2');
+sectionArrowToggle('#sch-card__data-toggle h3');
+
+/*     transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+      transition-duration: 0.7s;
+  transform: rotate(180deg);
+  -webkit-transform: rotate(180deg); */
+
     $("nav:first").accessibleMegaMenu({
         /* prefix for generated unique id attributes, which are required 
            to indicate aria-owns, aria-controls and aria-labelledby */
